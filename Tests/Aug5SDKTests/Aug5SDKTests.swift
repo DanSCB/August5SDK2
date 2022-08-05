5fb747f56bbc832c70a7e101176e2411238afa4d
Dan Schnabel <dans@clearbridgemobile.com>
2022-08-05 16:44:01 -0400

    Commit the SDK

---
+++ Tests/Aug5SDKTests/Aug5SDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Aug5SDK
+
+final class Aug5SDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Aug5SDK().theText, "Hello, World!")
+    }
+}
